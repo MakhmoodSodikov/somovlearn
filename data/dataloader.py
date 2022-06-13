7565da6b4729223cd58a69ff9d56a291cb424aea
flaymax <69513102+flaymax@users.noreply.github.com>
2022-06-13 21:37:43 +0300

    cifar-10 loader

--- data/dataloader.py
+++ data/dataloader.py
@@ -25,14 +25,14 @@ class DataLoader:
         os.system(cifar10_extract)
 
     def _get_data_batches(self):
-        raw_batch = self._unpickle(f"cifar-10-batches-py/data_batch_1")
+        raw_batch = self._unpickle(f"../data/cifar-10-batches-py/data_batch_1")
         batch_data = raw_batch[b'data']
         print(raw_batch.keys())
         batch_labels = raw_batch[b'labels']
         print(batch_labels[0])
 
         for i in range(2, 6):
-            raw_batch = self._unpickle(f"cifar-10-batches-py/data_batch_{i}")
+            raw_batch = self._unpickle(f"../data/cifar-10-batches-py/data_batch_{i}")
 
             batch_data = np.vstack((batch_data, raw_batch[b'data']))
             batch_labels = np.hstack((batch_labels, raw_batch[b'labels']))
@@ -52,18 +52,27 @@ class DataLoader:
 
         return np.array(data_ret), np.array(labels_ret)
 
+    def _one_hot(self, labels):
+        n_classes = 10
+        out = np.eye(n_classes)[labels]
+        out[out == 0] = -1
+        return out
+
     def load_cifar10(self):
-        if not os.path.exists('cifar-10-batches-py'):
-            self._download_cifar10()
+        # if not os.path.exists('data/cifar-10-batches-py'):
+        #     self._download_cifar10()
 
         data, labels = self._get_data_batches()
         data, labels = self._shuffle(data, labels)
         data, labels = self._split_batches(data, labels)
+        labels_hot = self._one_hot(labels)
 
-        return data, labels
+        return data, labels, labels_hot
 
 
 if __name__ == "__main__":
-    loader = DataLoader(batch_size=256)
-    x, y = loader.load_cifar10()
+    loader = DataLoader(batch_size=32)
+    x, y_hot, y = loader.load_cifar10()
     print(x.shape)
+    print(y[0][0])
+    print(y_hot[0][0])
