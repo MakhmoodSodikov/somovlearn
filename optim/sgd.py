ddc72878faef65c0fab73a77490f87c98c02f0ec
flaymax <69513102+flaymax@users.noreply.github.com>
2022-06-13 21:38:50 +0300

    base SGD optimizer

--- optim/sgd.py
+++ optim/sgd.py
@@ -1,9 +1,13 @@
 from .base import BaseOptim
-
+import numpy as np
 
 class SGDOptimizer(BaseOptim):
     def __init__(self, learning_rate):
-        self.lr = learning_rate
+        self._lr = learning_rate
 
-    def step(self):
-        pass
+    def step(self, weight, dweight):
+        # TODO: Problem is here
+        #dweight = dweight.T
+
+        new_weight = weight - self._lr * dweight.T
+        return new_weight
